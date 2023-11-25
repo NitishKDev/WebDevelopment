463edff17c0d3c293cb4e65afdb16ce0e47fe0af
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2023-11-26 02:40:44 +0530

    added node4.js
    serving html file

---
+++ node4.js
@@ -0,0 +1,11 @@
+//serving html files
+const http=require("http");
+const fs=require("fs");
+const filecontent=fs.readFileSync('htmm.html')
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.end(filecontent)
+})
+server.listen(80,'127.0.0.1',()=>{
+    console.log("Listening on port 80");
+})
