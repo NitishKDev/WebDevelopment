88e03b1d937ac7d77164328cc2d22292260f228f
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-04-24 20:25:01 +0530

    Update BMI.js
    Added custom messages to show the analysis to the user.

--- BMI.js
+++ BMI.js
@@ -2,7 +2,23 @@
 function BMI(w,h)
 {
     var bmi = w / (h*h);
-    return Math.round(bmi);
+    //bmi = Math.round(bmi);
+    if(bmi<18.5)
+    {
+         var interpretation = 'Your BMI is '+bmi+', so you are underweight.';
+         return interpretation;
+    }
+   
+     if(bmi>=18.5 && bmi <24.9)
+     {
+   var interpretation = 'Your BMI is '+bmi+', so you have a normal weight.';
+    return interpretation;
+     }
+     if(bmi>=24.9)
+     {
+    var interpretation = 'Your BMI is '+bmi+', so you are overweight.';
+    return interpretation;
+     }
 }
 var w = prompt("Enter weight in kg");
 var h = prompt("Enter height in meter");
