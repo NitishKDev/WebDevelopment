ff30ec0d1e23f41a8ef4aeaec369f511d1c5dc91
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-04-25 09:31:14 +0530

    added arrayGuestList.js
    Program to check whether that guest is invited to the party or not.

---
+++ arrayGuestList.js
@@ -0,0 +1,11 @@
+var name = prompt("Enter your name");
+var gList=["Jack","Frost","Joey","Sparrow","Kilua","Ron"];
+if(gList.includes(name))
+{
+    alert("Welcome!");
+}
+else{
+    alert("Maybe next time!")
+}
+//console.log(gList);
+//console.log(gList.length);
