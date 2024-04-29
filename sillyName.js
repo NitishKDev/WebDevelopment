0bc6da0afabd77cc3d049dea25b43ad11a45e5b4
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-04-29 08:30:58 +0530

    added sillyName.js
    using package(sillyName) to generate random names.
    1)npm init. 2)npm install sillyName(installing package). 3)write code to generate random names. 4)modify json add type module to use import.

---
+++ sillyName.js
@@ -0,0 +1,5 @@
+//var generateName = require('sillyname');
+//modify json add type module to use import.
+import generateName from "sillyname";
+var sillyName = generateName();
+console.log("My name is "+sillyName);
