b86ae33be40cac6fa5c06e758f17819686800a17
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-04-29 08:45:30 +0530

    added superhero.js
    learning node and using package superheroes to generate random name of superheroes.

---
+++ superhero.js
@@ -0,0 +1,8 @@
+//const superheroes = require('superheroes');
+import superheroes from 'superheroes';
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+const name = superheroes.random();
+console.log("I am "+name);
+//=> 'Spider-Ham'
+
