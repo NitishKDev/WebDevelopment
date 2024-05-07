34899e48fcaa20359059bef70ae2bf23ee949bd0
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-05-07 14:50:04 +0530

    added stringAll.js

---
+++ stringAll.js
@@ -0,0 +1,16 @@
+//template literal
+let name1="luffy";
+let name2= "one piece";
+let sent= `${name1} belongs to ${name2}`;
+console.log(sent);
+
+//methods
+console.log(name1.length);
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
+console.log(name1.slice(1));
+console.log(name1.slice(1,4));
+console.log(name1.replace("lu","tu"));
+console.log(name1.concat(" yo ",name2," hoho"));
+let na=' fff   ';
+console.log(na.trim());
