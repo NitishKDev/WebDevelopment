26e4031a0a167ec0dd38fb8012b9fc4fce7dd801
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-05-07 15:12:12 +0530

    added StringendsWith.js
    Program based on endsWith(), startsWith(), slice().

---
+++ StringendsWith.js
@@ -0,0 +1,12 @@
+//endsWith
+let sente="Hello Jack Welcome";
+let wrd = "Welcome";
+console.log(sente.endsWith(wrd));
+
+//startsWith
+console.log(sente.startsWith(wrd));
+console.log(sente.startsWith(wrd,11));
+
+//slice
+let amt = "Please give me 1000";
+console.log(amt.slice(15));
