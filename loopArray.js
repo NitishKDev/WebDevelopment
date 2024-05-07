ef8143f83044af55be3f73e9627eeeff8b0bdb6d
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-05-07 16:31:42 +0530

    added loopArray.js

---
+++ loopArray.js
@@ -0,0 +1,32 @@
+let num = [3,5,3,6,12];
+// for(let i = 0 ; i<num.length;i++)
+//     {
+//         console.log(num[i]);
+//     }
+
+// //forEach
+// num.forEach((ele)=>
+//     {
+//       console.log(ele*ele); 
+//     });
+
+
+// //array from
+// let name="jack";
+// let s=Array.from(name);
+// console.log(s);
+
+
+// //for...of
+// for(let i of num){
+//     console.log(i);
+// }
+
+
+//for...in
+for(let i in num){
+    console.log(i);
+}
+
+
+//Higher order array methods (map,filter and reduce)
