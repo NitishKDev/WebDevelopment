215e84f592b1eabca91c5133802bdd3645c2c0f4
NITISH KUMAR <124025562+NitishKDev@users.noreply.github.com>
2024-05-28 20:03:24 +0530

    added arrowfn1.js

---
+++ arrowfn1.js
@@ -0,0 +1,9 @@
+const greety = (name,count)=> {
+    console.log(`Hello ${name}, Good Evening!`)
+    for(let i = 0;i<count;i++)
+        {
+            console.log(`${name} ${i} `);
+        }
+}
+greety("Jack",3);
+
